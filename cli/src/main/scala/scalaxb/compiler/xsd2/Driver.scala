bdc45b639b4d5a454a54e69c654ff02b04d83dce
e.e d3si9n <eed3si9n@gmail.com>
2011-01-09 05:16:29 -0500

    added XML Schema parser.

---
+++ cli/src/main/scala/scalaxb/compiler/xsd2/Driver.scala
@@ -0,0 +1,84 @@
+/*
+ * Copyright (c) 2011 e.e d3si9n
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package scalaxb.compiler.xsd2
+
+import java.io.{Reader, PrintWriter}
+import java.net.{URI}
+import scala.xml.{Node, Elem}
+import scalaxb.compiler.{Module, Config, Snippet}
+import scalaxb._
+import Scalaxb._
+import xmlschema._
+import XDefaultXMLProtocol._
+
+class Driver extends Module { driver =>
+  import scala.xml.factory.{XMLLoader}
+  import javax.xml.parsers.SAXParser
+
+  type Schema = XSchema
+  type Context = SchemaContext
+
+  def generate(schema: Schema, context: Context, config: Config): Snippet = {
+    Snippet(<source/>)
+  }
+
+  def generateProtocol(snippet: Snippet,
+    context: Context, config: Config): Seq[Node] = Seq(<source/>)
+
+  def toImportable(location: URI, in: Reader, out: PrintWriter): Importable = {
+    new Importable {
+      lazy val reader: Reader = in
+      lazy val out: PrintWriter = out
+      lazy val location: URI = location
+      lazy val elem = CustomXML.load(reader)
+
+      def targetNamespace: Option[String] = None
+      def importNamespaces: Seq[String] = Nil
+      def importLocations: Seq[String] = Nil
+      def includeLocations: Seq[String] = Nil
+
+      def toSchema(context: Context): Schema = {
+        val schema = fromXML[XSchema](elem)
+        log("Driver.toSchema: " + schema.toString())
+        schema
+      }
+  }
+  }
+
+  def buildContext: Context = SchemaContext()
+
+  def processContext(context: Context, config: Config) {
+    // do nothing.
+  }
+
+  object CustomXML extends XMLLoader[Elem] {
+    override def parser: SAXParser = {
+      val factory = javax.xml.parsers.SAXParserFactory.newInstance()
+      factory.setFeature("http://xml.org/sax/features/validation", false)
+      factory.setFeature("http://apache.org/xml/features/disallow-doctype-decl", false)
+      factory.setFeature("http://apache.org/xml/features/nonvalidating/load-dtd-grammar", false)
+      factory.setFeature("http://apache.org/xml/features/nonvalidating/load-external-dtd", false)
+      factory.newSAXParser()
+    }
+  }
+}
